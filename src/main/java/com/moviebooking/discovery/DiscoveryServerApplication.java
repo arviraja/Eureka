2d335e4b2397b1bfe329b3f877e2b42a51b9eea6
arviraja <arvindr369@gmail.com>
2026-02-28 03:00:31 +0530

    made changes according to version change

--- src/main/java/com/moviebooking/discovery/DiscoveryServerApplication.java
+++ src/main/java/com/moviebooking/discovery/DiscoveryServerApplication.java
@@ -2,11 +2,10 @@ package com.moviebooking.discovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@EnableEurekaServer
 @SpringBootApplication
+@EnableEurekaServer
 public class DiscoveryServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(DiscoveryServerApplication.class, args);
